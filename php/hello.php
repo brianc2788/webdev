3552c95e2f28204593306932d738c996f7f5a21d
brianc2788 <brianc2788@gmail.com>
2022-05-16 15:04:22 -0400

    Huge commit - Organizational overhaul.

---
+++ php/hello.php
@@ -0,0 +1,4 @@
+<?php
+$var1 =  'hey there';
+echo "$var1\n";
+?>
---
+++ ruby/cowsays.rb
@@ -0,0 +1,10 @@
+#ruby comment
+def add(num1,num2)
+	print("the cow says...\nI mean, two plus two is....!!!\n");
+	return num1 + num2;
+end
+
+nNum1 = 2;
+nNum2 = 2;
+
+print(add(nNum1,nNum2),"\n");
