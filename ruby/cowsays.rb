364bddfe28cfa80dfd4574b70adf899e619f6b78
brianc2788 <brianc2788@gmail.com>
2022-07-05 19:43:28 -0400

    General restructuring.

--- ruby/cowsays.rb
+++ ruby/cowsays.rb
@@ -1,10 +1,13 @@
-#ruby comment
+# what does the cow say?
+# i mean, add a couple numbers.
+# i know rubyyyy.
 def add(num1,num2)
+	nSum = num1 + num2;
 	print("the cow says...\nI mean, two plus two is....!!!\n");
-	return num1 + num2;
+	print(nSum);
 end
 
 nNum1 = 2;
 nNum2 = 2;
 
-print(add(nNum1,nNum2),"\n");
+add(nNum1,nNum2);
